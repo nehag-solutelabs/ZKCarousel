477dd861c7a342d6cad3ce29ad0792e0979a96f1
Neha Gupta <neha.g@solutelabs.com>
2021-12-28 00:07:48 +0530

    increase top

--- ZKCarousel/Classes/ZKCarouselCell.swift
+++ ZKCarousel/Classes/ZKCarouselCell.swift
@@ -93,9 +93,9 @@ public class ZKCarouselCell: UICollectionViewCell {
         titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16).isActive = true
         titleLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16).isActive = true
         titleLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: 8).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 55).isActive = true
         titleLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -8).isActive = true
-//        titleLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
     }
     
     private func parseData(forSlide slide: ZKCarouselSlide) {
