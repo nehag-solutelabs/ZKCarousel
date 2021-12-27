0556293e379afb0d03e002050b07d4dc620b51f1
Neha Gupta <neha.g@solutelabs.com>
2021-12-27 23:20:03 +0530

    revert title label contraints

--- ZKCarousel/Classes/ZKCarouselCell.swift
+++ ZKCarousel/Classes/ZKCarouselCell.swift
@@ -93,7 +93,8 @@ public class ZKCarouselCell: UICollectionViewCell {
         titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 16).isActive = true
         titleLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -16).isActive = true
         titleLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: 8).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35).isActive = true
+//        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 35).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: descriptionLabel.topAnchor, constant: -8).isActive = true
         titleLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
     }
     
